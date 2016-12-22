dd0386f17f24b579f6fa26b455f6984c1531bf1a
devilesk <devilesk@gmail.com>
2016-12-22 15:19:51 -0500

    add uglify compress and mangle options

--- www/bundle.js
+++ www/bundle.js
@@ -864,7 +864,6 @@ function VisionSimulation(worlddata, mapDataImagePath, onReady, opts) {
     }
 }
 VisionSimulation.prototype.updateVisibility = function (gX, gY) {
-    var t3 = Date.now();
     var self = this,
         key = xy2key(gX, gY),
         elevation = this.elevationGrid[key].z,
@@ -875,9 +874,7 @@ VisionSimulation.prototype.updateVisibility = function (gX, gY) {
     setWalls(this.walls, this.ent_fow_blocker_node);
     setWalls(this.walls, this.tools_no_wards);
     setTreeWalls(this.walls, elevation, this.tree, this.tree_elevations, this.tree_state, this.tree_blocks);
-    var t4 = Date.now();
-    console.log(t4 - t3);
-    var t1 = Date.now();
+
     fov.walls = this.walls;
     this.lights = {};
     fov.compute(gX, gY, this.radius, function(x2, y2, r, vis) {
@@ -886,8 +883,6 @@ VisionSimulation.prototype.updateVisibility = function (gX, gY) {
             self.lights[key] = 255;
         }
     });
-    var t2 = Date.now();
-    console.log(t2 - t1);
 }
 
 VisionSimulation.prototype.toggleTree = function (x, y) {
