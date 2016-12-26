8afa5dcf73cd99afcdbf2ffcf6db0108e905bb3a
devilesk <devilesk@gmail.com>
2016-12-26 16:33:00 -0500

    update perf

--- test/perf.js
+++ test/perf.js
@@ -2,20 +2,22 @@ var VisionSimulation = require("../src/vision-simulation.js");
 var worlddata = require("../src/worlddata.json");
 
 var vs = new VisionSimulation(worlddata, './www/map_data.png', function () {
+    var total = 0;
     for (var i = 0; i < 10; i++) {
-        console.log('run', i);
-        profile();
+        var c = profile();
+        console.log('run', i, c + 'ms');
+        total += c
     }
+    console.log('average run', total/10 + 'ms');
 });
 
 function profile() {
     var t1 = Date.now();
     for (var i = 0; i < vs.gridWidth; i+=20) {
-        var t2 = Date.now();
         for (var j = 0; j < vs.gridHeight; j+=20) {
             vs.updateVisibility(i, j);
         }
-        console.log(Date.now() - t2 + 'ms');
     }
-    console.log(Date.now() - t1 + 'ms');
+    var t2 = Date.now()
+    return t2 - t1;
 }
